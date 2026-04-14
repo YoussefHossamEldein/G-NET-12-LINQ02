47dbda0399d7fd7f1c839e59d7f438fa00d84544
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-14 16:47:01 +0200

    Add project files.

---
+++ Assignment02LinQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02LinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
